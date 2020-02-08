5c82e86a1c8af8fc2a4839b6b0ccaf3d04abaac7
stephen <spl15@students.uwf.edu>
2020-02-08 13:33:22 -0600

    testing

--- test/client.c
+++ test/client.c
@@ -1,42 +1,26 @@
-
-// Client side C/C++ program to demonstrate Socket programming 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <string.h> 
-#define PORT 60019 
-   
-int main(int argc, char *argv[]) 
-{ 
-    int sock = 0, valread; 
-    struct sockaddr_in serv_addr; 
-    char *hello = argv[1];
-    char buffer[1024] = {0}; 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\n Socket creation error \n"); 
-        return -1; 
-    } 
-   
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(PORT); 
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
-    { 
-        printf("\nInvalid address/ Address not supported \n"); 
-        return -1; 
-    } 
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection Failed \n"); 
-        return -1; 
-    } 
-    send(sock , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    valread = read( sock , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    return 0; 
-} 
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<stdio.h>
+#include <strings.h>
+#include <stdlib.h>
+#define SA struct sockaddr
+int main(int argc,char**argv)
+{
+int sockfd;
+char fname[25];
+int len;
+struct sockaddr_in servaddr,cliaddr;
+sockfd=socket(AF_INET,SOCK_STREAM,0);
+bzero(&servaddr,sizeof(servaddr));
+servaddr.sin_family=AF_INET;
+servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+servaddr.sin_port=htons(atoi(argv[1]));
+inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
+connect(sockfd,(SA*)&servaddr,sizeof(servaddr));
+char buffer[100];
+FILE *f;
+f=fopen("add.txt","r");
+fscanf(f,"%s",buffer);
+write(sockfd,buffer,100);
+printf("the file was sent successfully");
+}
